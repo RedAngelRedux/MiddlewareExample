258f32c836d4894dafa8ff107e88a2e226d7012b
RedAngelRedux <sammy@sammynava.com>
2023-08-26 16:22:17 -0600

    Create and use a Custom Middleware Class.

---
+++ MiddlewareEx/Middleware/MiddlewareTwo.cs
@@ -0,0 +1,12 @@
+﻿namespace MiddlewareEx.Middleware
+{
+    public class MiddlewareTwo : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Start of MiddlewareTwo. ");
+            await next(context);
+            await context.Response.WriteAsync("End of MiddlewareTwo. ");
+        }
+    }
+}
--- MiddlewareEx/Program.cs
+++ MiddlewareEx/Program.cs
@@ -1,15 +1,88 @@
+/// </example_three>
+/// Example of a chained middleware.  In this example we use app.Use(), which can 
+/// forward the context to the next middleware in the chain, and/or terminate the
+/// chaid as well
+///
+using MiddlewareEx.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<MiddlewareTwo>();
+
 var app = builder.Build();
 
-app.Run(async (HttpContext context) =>
+// middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello.");
-    await context.Response.WriteAsync(" Hello World.");
+    await context.Response.WriteAsync("Hello from Middleware 1. ");
+    await next(context);
 });
 
+// middleware 2
+app.UseMiddleware<MiddlewareTwo>();
+
+// middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello Again World.");
+    await context.Response.WriteAsync("Hello from middleware 3. ");
 });
+///
+/// </example_three>
+
+
+///// </example two>
+///// Example of a chained middleware.  In this example we use app.Use(), which can 
+///// forward the context to the next middleware in the chain, and/or terminate the
+///// chaid as well
+/////
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//// middleware 1
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Hello from middleware 1. ");
+//    await next(context);
+//});
+
+//// middleware 2
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Hello from middleware 2. ");
+//    await next(context);
+//});
+
+//// middleware 3
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello from middleware 3");
+//});
+/////
+///// </example two>
+
+///// <example_one> 
+///// Example of short-circuited Middleware.  In this case the second app.Run() will
+///// never be called.
+/////
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//// middleware 1
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello from Middleware 1. ");
+//});
+
+//// middleware 2
+///// <note>
+///// This method is never called; it is short-circuited because previous middleware 
+///// was a terminating middleware
+///// </note>
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello from Middleware 2.");
+//});
+/////
+///// </example_one>
 
 app.Run();
