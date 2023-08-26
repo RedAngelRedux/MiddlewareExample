83d4b0d1a1718cc224e4bedd6b63f0ba593ceee3
RedAngelRedux <sammy@sammynava.com>
2023-08-26 17:18:26 -0600

    Implement and test a Middleware Extension Method.

--- MiddlewareEx/Middleware/MiddlewareTwo.cs
+++ MiddlewareEx/Middleware/MiddlewareTwo.cs
@@ -4,9 +4,17 @@
     {
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await context.Response.WriteAsync("Start of MiddlewareTwo. ");
+            await context.Response.WriteAsync("Start of MiddlewareTwo.\n");
             await next(context);
-            await context.Response.WriteAsync("End of MiddlewareTwo. ");
+            await context.Response.WriteAsync("End of MiddlewareTwo.\n");
+        }
+    }
+
+    public static class MiddlewareTowExtension
+    {
+        public static IApplicationBuilder UseMiddlewareTwo(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MiddlewareTwo>();
         }
     }
 }
--- MiddlewareEx/Program.cs
+++ MiddlewareEx/Program.cs
@@ -1,7 +1,6 @@
-/// </example_three>
-/// Example of a chained middleware.  In this example we use app.Use(), which can 
-/// forward the context to the next middleware in the chain, and/or terminate the
-/// chaid as well
+/// </example_four>
+/// This example uses an Extension method to simply the syntax and follow .NET 
+/// convention
 ///
 using MiddlewareEx.Middleware;
 
@@ -14,20 +13,52 @@ var app = builder.Build();
 // middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello from Middleware 1. ");
+    await context.Response.WriteAsync("Hello from Middleware 1.\n");
     await next(context);
 });
 
 // middleware 2
-app.UseMiddleware<MiddlewareTwo>();
+app.UseMiddlewareTwo();
 
 // middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello from middleware 3. ");
+    await context.Response.WriteAsync("Hello from middleware 3.\n");
 });
 ///
-/// </example_three>
+/// </example_four>
+
+
+///// </example_three>
+///// Example of a chained middleware.  In this example we use app.Use(), which can 
+///// forward the context to the next middleware in the chain, and/or terminate the
+///// chaid as well
+/////
+//using MiddlewareEx.Middleware;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddTransient<MiddlewareTwo>();
+
+//var app = builder.Build();
+
+//// middleware 1
+//app.Use(async (HttpContext context, RequestDelegate next) =>
+//{
+//    await context.Response.WriteAsync("Hello from Middleware 1. ");
+//    await next(context);
+//});
+
+//// middleware 2
+//app.UseMiddleware<MiddlewareTwo>();
+
+//// middleware 3
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("Hello from middleware 3. ");
+//});
+/////
+///// </example_three>
 
 
 ///// </example two>
