f5b062d4b6b56aaf09548907a0b4a192f805975c
RedAngelRedux <sammy@sammynava.com>
2023-08-26 14:25:10 -0600

    Middleware Example 1
    Illustrate adding middleware via a lambda expression in app.Run() in Program.cs

--- MiddlewareEx/Program.cs
+++ MiddlewareEx/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello.");
+    await context.Response.WriteAsync(" Hello World.");
+});
+
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello Again World.");
+});
 
 app.Run();
